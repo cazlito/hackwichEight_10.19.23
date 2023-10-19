4ca387d9d8d5b6a35cbeb3da4df3cba314b21e03
cazlito <142947671+cazlito@users.noreply.github.com>
2023-10-19 12:59:09 -1000

    complete setting labels

--- hackwichEight_10.19.23/ViewController.swift
+++ hackwichEight_10.19.23/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var mylabel: UILabel!
+    
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex
+            {
+        case 0: mylabel.text = "First Segment has been selected"
+            
+        case 1: mylabel.text = "Second Segment has been selected"
+               
+        default:break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
